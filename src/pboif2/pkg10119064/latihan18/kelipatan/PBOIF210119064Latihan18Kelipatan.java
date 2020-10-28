37dd04b09ec9851a52236bb3e4bd7c8c9366dca5
Bagus_24 <Bagus_24@Bagus24>
2020-10-28 08:55:49 +0700

    new commit

---
+++ src/pboif2/pkg10119064/latihan18/kelipatan/PBOIF210119064Latihan18Kelipatan.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119064.latihan18.kelipatan;
+
+/**
+ *
+ * @author Bagus_24
+ * NAMA         : Bagus Budi Wibowo
+ * KELAS        : PBOIF2 
+ * NIM          : 10119064 
+ * Deskripsi Program : Program ini berisi untuk menghitung kelipatan nilai 3.5
+ */
+public class PBOIF210119064Latihan18Kelipatan {
+    
+    public static void kelipatan(){
+        for (double i = 3.5; i <= 35;) {
+            System.out.println(i);
+            i += 3.5;
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        kelipatan();
+    }
+    
+}
